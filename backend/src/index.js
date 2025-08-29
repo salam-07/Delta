db013cc57aabffa6456ba798ab90969838909d9e
salam-07 <abdussalamblue7@gmail.com>
2025-08-29 19:10:04 +0500

    Update development scripts in package.json and modify CORS origin to an array format

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 // cors policy for dev
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173"],
         credentials: true
     }
 ));
